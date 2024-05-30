5d5588b5b47ba905d299b8b8852ee2e18cf22ecf
nhodd-plato <nick.hodd@platotech.com>
2024-05-30 09:32:23 -0300

    Done aesthetically testing My Profile page.

--- src/main/java/org/example/locators/MyProfile.java
+++ src/main/java/org/example/locators/MyProfile.java
@@ -34,20 +34,96 @@ public class MyProfile {
         return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/div/ul/li[1]/a/span[2]";
     }
 
+    //Post sub tab
     public static String postSubTab(){
         return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section[1]/div[1]/div/div/ul/li[1]/a";
     }
 
+    public static String postSubTabShareAnUpdate(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section/div[1]/div/section/div/div[1]/button[1]";
+    }
+
+    public static String postSubTabShareButton(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section/div[1]/div/section/div/div[1]/button[2]/span";
+    }
+
+    //Poll sub tab
     public static String pollSubTab(){
         return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section[1]/div[1]/div/div/ul/li[2]/a/span[2]";
     }
 
+//    public static String pollSubTabQuestion(){
+//        return "//*[@id=\"outerContainer\"]/div/label/";
+//    }
+
+    public static String pollSubTabChoiceOne(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section/div[1]/div/section[2]/div/div[3]/div/div/div/div[1]/section/div/ul/li[1]/div/div/label/span";
+    }
+
+    public static String pollSubTabChoiceTwo(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section/div[1]/div/section[2]/div/div[3]/div/div/div/div[1]/section/div/ul/li[2]/div/div/label/span";
+    }
+
+    public static String pollSubAddChoiceButton(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section/div[1]/div/section[2]/div/div[3]/div/div/div/div[2]/div[1]/button";
+    }
+
+    public static String pollSubTabAskButton(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section/div[1]/div/section[2]/div/div[3]/div/div/div/div[2]/div[2]/button";
+    }
+
+    // Question sub tab
     public static String questionSubTab(){
         return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section[1]/div[1]/div/div/ul/li[3]/a/span[2]";
     }
 
+    public static String questionSubTabQuestion(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section/div[1]/div/section[3]/div/div[3]/div/div/div/div[1]/div[3]/div[1]/div[1]/div/label/span[2]";
+    }
+
+    public static String questionSubTabDetails(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section/div[1]/div/section[3]/div/div[3]/div/div/div/div[1]/div[3]/div[1]/div[2]/div/div/div/div/div[1]/div[1]/span/span";
+    }
+
+    public static String questionSubTabAskButton(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section/div[1]/div/section[3]/div/div[3]/div/div/div/div[2]/div[2]/button";
+    }
+
+    //Cases tab
     public static String casesTab(){
         return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/div/ul/li[2]/a/span[2]";
+    }
+
+    public static String myCases(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section[2]/div/div[1]/div[1]";
+    }
+
+    public static String casesNumber(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section[2]/div/div[2]/div/div[1]/table/thead/tr/th[1]/a";
+    }
+
+    public static String contactName(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section[2]/div/div[2]/div/div[1]/table/thead/tr/th[2]/a";
+    }
+
+    public static String subject(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section[2]/div/div[2]/div/div[1]/table/thead/tr/th[3]/a";
+    }
+
+    public static String status(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section[2]/div/div[2]/div/div[1]/table/thead/tr/th[4]/a";
+    }
+
+    public static String priority(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section[2]/div/div[2]/div/div[1]/table/thead/tr/th[5]/a";
+    }
+
+    public static String date(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section[2]/div/div[2]/div/div[1]/table/thead/tr/th[6]/a";
+    }
+
+    public static String caseOwner(){
+        return "/html/body/div[3]/div[2]/div/div[2]/div/div/div/div/div[1]/div[2]/div/div[2]/div/section[2]/div/div[2]/div/div[1]/table/thead/tr/th[7]/a";
     }
 
     public static String createCaseLink(){
--- src/test/java/tests/sanity/MyProfileTest.java
+++ src/test/java/tests/sanity/MyProfileTest.java
@@ -26,7 +26,7 @@ public class MyProfileTest {
     }
 
     @Test (priority = 1)
-    public void checkProfileIcon(){
+    public void profileIcon(){
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         WebElement profileIcon = driver.findElement(By.xpath(MyProfile.profileIcon()));
         if(profileIcon == null){
@@ -35,76 +35,188 @@ public class MyProfileTest {
     }
 
     @Test (priority = 2)
-    public void checkEditButton(){
+    public void editButton(){
         String editButton = driver.findElement(By.xpath(MyProfile.editButton())).getText();
         Assert.assertEquals(editButton, "Edit");
     }
 
     @Test (priority = 3)
-    public void checkInfluenceText(){
+    public void influenceText(){
         String influence = driver.findElement(By.xpath(MyProfile.influence())).getText();
         Assert.assertEquals(influence, "Influence");
     }
 
     @Test (priority = 4)
-    public void checkGroupsText(){
+    public void groupsText(){
         String groups = driver.findElement(By.xpath(MyProfile.groups())).getText();
         Assert.assertEquals(groups, "Groups");
     }
 
     @Test (priority = 5)
-    public void checkFilesText(){
+    public void filesText(){
         String files = driver.findElement(By.xpath(MyProfile.files())).getText();
         Assert.assertEquals(files, "Files");
     }
 
     @Test (priority = 6)
-    public void checkFollowersText(){
+    public void followersText(){
         String followers = driver.findElement(By.xpath(MyProfile.followers())).getText();
         Assert.assertEquals(followers, "Followers");
     }
 
     @Test (priority = 7)
-    public void checkFollowingText(){
+    public void followingText(){
         String following = driver.findElement(By.xpath(MyProfile.following())).getText();
         Assert.assertEquals(following, "Following");
     }
 
     @Test (priority = 8)
-    public void checkFeedTabText(){
+    public void feedTabText(){
         String feedTab = driver.findElement(By.xpath(MyProfile.feedTab())).getText();
         Assert.assertEquals(feedTab, "FEED");
     }
 
     @Test (priority = 9)
-    public void checkPostSubTabText(){
+    public void postSubTabText(){
         String postSubTab = driver.findElement(By.xpath(MyProfile.postSubTab())).getText();
         Assert.assertEquals(postSubTab, "Post");
     }
 
     @Test (priority = 10)
-    public void checkPollSubTabText(){
-        String pollSubTab = driver.findElement(By.xpath(MyProfile.pollSubTab())).getText();
-        Assert.assertEquals(pollSubTab, "Poll");
+    public void postSubTabTextbox(){
+        String postSubTabShareAnUpdate = driver.findElement(By.xpath(MyProfile.postSubTabShareAnUpdate())).getText();
+        Assert.assertEquals(postSubTabShareAnUpdate, "Share an update...");
     }
 
     @Test (priority = 11)
-    public void checkQuestionSubTabText(){
-        String questionSubTab = driver.findElement(By.xpath(MyProfile.questionSubTab())).getText();
-        Assert.assertEquals(questionSubTab, "Question");
+    public void postSubTabShareButton(){
+        String postSubTabShareButton = driver.findElement(By.xpath(MyProfile.postSubTabShareButton())).getText();
+        Assert.assertEquals(postSubTabShareButton, "Share");
     }
 
     @Test (priority = 12)
-    public void checkCasesTabText(){
+    public void pollSubTabText(){
+        String pollSubTab = driver.findElement(By.xpath(MyProfile.pollSubTab())).getText();
+        driver.findElement(By.xpath(MyProfile.pollSubTab())).click();
+        Assert.assertEquals(pollSubTab, "Poll");
+    }
+
+    // the element uses a dynamic value every time the page is loaded so not sure how to access this
+//    @Test (priority = 12)
+//    public void pollSubTabQuestionText(){
+//        String pollSubTabQuestion = driver.findElement(By.xpath(MyProfile.pollSubTabQuestion())).getText();
+//        Assert.assertEquals(pollSubTabQuestion, "Question");
+//    }
+
+    @Test (priority = 13)
+    public void pollSubTabChoiceOne(){
+        String pollSubTabChoiceOne = driver.findElement(By.xpath(MyProfile.pollSubTabChoiceOne())).getText();
+        Assert.assertEquals(pollSubTabChoiceOne, "Choice 1");
+    }
+
+    @Test (priority = 14)
+    public void pollSubTabChoiceTwo(){
+        String pollSubTabChoiceTwo = driver.findElement(By.xpath(MyProfile.pollSubTabChoiceTwo())).getText();
+        Assert.assertEquals(pollSubTabChoiceTwo, "Choice 2");
+    }
+
+    @Test (priority = 15)
+    public void pollSubTabAddChoiceButton(){
+        String pollSubAddChoiceButton = driver.findElement(By.xpath(MyProfile.pollSubAddChoiceButton())).getText();
+        Assert.assertEquals(pollSubAddChoiceButton, "Add new choice");
+    }
+
+    @Test (priority = 16)
+    public void pollSubTabAskButton(){
+        String pollSubTabAskButton = driver.findElement(By.xpath(MyProfile.pollSubTabAskButton())).getText();
+        driver.findElement(By.xpath(MyProfile.pollSubTab())).click();
+        Assert.assertEquals(pollSubTabAskButton, "Ask");
+    }
+
+    @Test (priority = 17)
+    public void questionSubTabText(){
+        String questionSubTab = driver.findElement(By.xpath(MyProfile.questionSubTab())).getText();
+        driver.findElement(By.xpath(MyProfile.questionSubTab())).click();
+        Assert.assertEquals(questionSubTab, "Question");
+    }
+
+    @Test (priority = 18)
+    public void questionSubTabQuestionText(){
+        String questionSubTabQuestion = driver.findElement(By.xpath(MyProfile.questionSubTabQuestion())).getText();
+        Assert.assertEquals(questionSubTabQuestion, "Question (Enter up to 255 characters)");
+    }
+
+    @Test (priority = 19)
+    public void questionSubTabDetailsText(){
+        String questionSubTabDetails = driver.findElement(By.xpath(MyProfile.questionSubTabDetails())).getText();
+        Assert.assertEquals(questionSubTabDetails, "Details");
+    }
+
+    @Test (priority = 19)
+    public void questionSubTabAskButton(){
+        String questionSubTabAskButton = driver.findElement(By.xpath(MyProfile.questionSubTabAskButton())).getText();
+        Assert.assertEquals(questionSubTabAskButton, "Ask");
+    }
+
+    @Test (priority = 20)
+    public void casesTabText(){
         String casesTab = driver.findElement(By.xpath(MyProfile.casesTab())).getText();
         driver.findElement(By.xpath(MyProfile.casesTab())).click();
         Assert.assertEquals(casesTab, "CASES");
     }
 
-    @Test (priority = 13)
-    public void checkCreateCaseLinkText(){
+    @Test (priority = 21)
+    public void casesTabMyCases(){
+        String myCases = driver.findElement(By.xpath(MyProfile.myCases())).getText();
+        Assert.assertEquals(myCases, "My Cases");
+    }
+
+    @Test (priority = 22)
+    public void casesTabCreateCaseLinkText(){
         String createCaseLink = driver.findElement(By.xpath(MyProfile.createCaseLink())).getText();
         Assert.assertEquals(createCaseLink, "Create Case");
+    }
+
+    @Test (priority = 23)
+    public void casesTabCaseNumber(){
+        String casesNumber = driver.findElement(By.xpath(MyProfile.casesNumber())).getText();
+        Assert.assertTrue(casesNumber.contains("Case Number"));
+    }
+
+    @Test (priority = 24)
+    public void casesTabContactName(){
+        String contactName = driver.findElement(By.xpath(MyProfile.contactName())).getText();
+        Assert.assertTrue(contactName.contains("Contact Name"));
+    }
+
+    @Test (priority = 25)
+    public void casesTabSubject(){
+        String subject = driver.findElement(By.xpath(MyProfile.subject())).getText();
+        Assert.assertTrue(subject.contains( "Subject"));
+    }
+
+    @Test (priority = 26)
+    public void casesTabStatus(){
+        String status = driver.findElement(By.xpath(MyProfile.status())).getText();
+        Assert.assertTrue(status.contains("Status"));
+    }
+
+    @Test (priority = 27)
+    public void casesTabPriority(){
+        String priority = driver.findElement(By.xpath(MyProfile.priority())).getText();
+        Assert.assertTrue(priority.contains("Priority"));
+    }
+
+    @Test (priority = 28)
+    public void casesTabDate(){
+        String date = driver.findElement(By.xpath(MyProfile.date())).getText();
+        Assert.assertTrue(date.contains("Date/Time Opened"));
+    }
+
+    @Test (priority = 29)
+    public void casesTabCaseOwner(){
+        String caseOwner = driver.findElement(By.xpath(MyProfile.caseOwner())).getText();
+        Assert.assertTrue(caseOwner.contains( "Case Owner Alias"));
     }
 
     @AfterClass
