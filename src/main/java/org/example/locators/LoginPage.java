5b7018c1d645005b4bf53066864bb7f4d1921216
SandeepDua <sandeep.dua@platotech.com>
2024-06-14 16:10:48 -0400

    Test

--- src/main/java/org/example/locators/LoginPage.java
+++ src/main/java/org/example/locators/LoginPage.java
@@ -3,12 +3,9 @@ package org.example.locators;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
 import java.time.Duration;
 
-/*
- This file contains all the locators associated with the login page
- needed to log in. as well as a log in method.
-*/
 public class LoginPage{
 
     public String userName(){
@@ -23,19 +20,13 @@ public class LoginPage{
         return "//*[@id=\"centerPanel\"]/div/div[2]/div/div[2]/div/div[3]/button";
     }
 
-    /*
-    This method logs into the website and is used by every test
-    **********NOTE: YOU HAVE TO ENTER YOUR OWN LOGIN CREDENTIALS***********
-    @param driver: is the webDriver created in each test and used to log in
-    @param loginPage: is the login object used to access login elements
-    */
     public void login(WebDriver driver, LoginPage loginPage){
         driver.get("https://brave-unicorn-kbiupz-dev-ed.trailblaze.my.site.com/s/login/");
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 
         WebElement userName = driver.findElement(By.xpath(loginPage.userName()));
-        userName.sendKeys("<username>");
+        userName.sendKeys("nick.hodd@platotech.com");
 
         WebElement password = driver.findElement(By.xpath(loginPage.password()));
         password.sendKeys("<password>");
